ddbb624993bb9f51db8d6ea91a9648ced174c52e
MarcelDias321 <78244792+MarcelDias321@users.noreply.github.com>
2022-04-26 23:14:23 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b9264b2942e615', '3027fe67', 'heroku_a1e6997f636ff88') or die('Erro ao conectar ao banco de dados');
